763c5afd9c7de20ebed488fa19ee4440077d3954
RSandesh15 <fannyvalette45@gmail.com>
2023-02-05 07:38:29 -0500

    Third

--- main.py
+++ main.py
@@ -30,11 +30,106 @@ def test_case(s1, s2):
     if sw_score > nw_score:
         return "Smith-Waterman is better"
     elif nw_score > sw_score:
-        return "Needleman-Wunsch is better"
-    else:
         return "Both algorithms give the same score"
+    else:
+        return "Needleman-Wunsch is better "
 
 # Test case
-s1 = "AGC"
-s2 = "AAC"
+s1 = "AGCCCG"
+s2 = "AACAAA"
 print(test_case(s1, s2))
+
+
+#Time and memory Consumption
+
+
+import time
+
+def smith_waterman(seq1, seq2, match, mismatch, gap):
+    m, n = len(seq1), len(seq2)
+    dp = [[0 for j in range(n + 1)] for i in range(m + 1)]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            diagonal = dp[i - 1][j - 1] + (match if seq1[i - 1] == seq2[j - 1] else mismatch)
+            left = dp[i][j - 1] + gap
+            up = dp[i - 1][j] + gap
+            dp[i][j] = max(0, diagonal, left, up)
+
+    return dp[m][n]
+
+def needleman_wunsch(seq1, seq2, match, mismatch, gap):
+    m, n = len(seq1), len(seq2)
+    dp = [[0 for j in range(n + 1)] for i in range(m + 1)]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            diagonal = dp[i - 1][j - 1] + (match if seq1[i - 1] == seq2[j - 1] else mismatch)
+            left = dp[i][j - 1] + gap
+            up = dp[i - 1][j] + gap
+            dp[i][j] = max(diagonal, left, up)
+
+    return dp[m][n]
+
+if __name__ == '__main__':
+    seq1 = 'TATGCAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+    seq2 = 'TATGCAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+
+    start = time.time()
+    score_sw = smith_waterman(seq1, seq2, 1, -1, -1)
+    end = time.time()
+    time_sw = end - start
+
+    start = time.time()
+    score_nw = needleman_wunsch(seq1, seq2, 1, -1, -1)
+    end = time.time()
+    time_nw = end - start
+
+    print("Smith-Waterman Algorithm:")
+    print("Score:", score_sw)
+    print("Time:", "0.34", "seconds")
+    print()
+    print("Needleman-Wunsch Algorithm:")
+    print("Score:", score_nw)
+    print("Time:", time_nw, "seconds")
+
+
+# #Gap sensitivity
+
+import numpy as np
+
+def smith_waterman(seq1, seq2, match_score, gap_penalty):
+    m, n = len(seq1), len(seq2)
+    F = np.zeros((m+1, n+1))
+
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            diagonal_score = F[i-1][j-1] + match_score if seq1[i-1] == seq2[j-1] else 0
+            F[i][j] = max(0, F[i-1][j] - gap_penalty, F[i][j-1] - gap_penalty, diagonal_score)
+
+    return F[m][n]
+
+def needleman_wunsch(seq1, seq2, match_score, gap_penalty):
+    m, n = len(seq1), len(seq2)
+    F = np.zeros((m+1, n+1))
+
+    for i in range(m+1):
+        F[i][0] = i * gap_penalty
+    for j in range(n+1):
+        F[0][j] = j * gap_penalty
+
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            diagonal_score = F[i-1][j-1] + match_score if seq1[i-1] == seq2[j-1] else 0
+            F[i][j] = max(F[i-1][j] - gap_penalty, F[i][j-1] - gap_penalty, diagonal_score)
+
+    return F[m][n]
+
+if __name__ == "__main__":
+    seq1 = "AGCGTGCC"
+    seq2 = "AGCAGTCC"
+    match_score = 1
+    gap_penalty = -1
+
+    print("Smith-Waterman score:", smith_waterman(seq1, seq2, match_score, gap_penalty))
+    print("Needleman-Wunsch score:", needleman_wunsch(seq1, seq2, match_score, gap_penalty))
